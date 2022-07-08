8d72f82e6e7e0c0dbe1380e5bb0335f0eeaef348
Rayron Soares <100003639+Rayron2012@users.noreply.github.com>
2022-07-08 20:12:41 -0300

    Deploy jun
    Primeiro deploy em que estou analisando o pycharm uma escolha mais dificil e pratica que o jupyter para data science, penso em mudar para o anaconda em breve

---
+++ Projeto 1 Analisar engajamento/Analisando a Tag.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+base = pd.read_excel("08. Analisando o engajamento no Instagram.xlsx")
+
--- Projeto 1 Analisar engajamento/Importando e tratando a base com Pandas.py
+++ Projeto 1 Analisar engajamento/Importando e tratando a base com Pandas.py
@@ -13,7 +13,7 @@ Tags vazias é que realmente não possuem tag (favor tratar como vazio)'''
 
 #  importar a base com pandas
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 base = pd.read_excel("08. Analisando o engajamento no Instagram.xlsx")
 
@@ -44,6 +44,30 @@ base.groupby('Tipo')["Curtidas"].mean()
 '''Estrutura : após o groupby, inserir a categoria que quer agrupar e entre colchetes a informação que deseja que seja 
 agrupada, e no final a função de agregação'''
 
+# print(base.groupby(['Tipo', 'Pessoas', 'Campanhas'])[["Curtidas", "Comentários"]].mean())
+'''Nessa estrutura a com mais de uma coluna temos que utilizar [] dentro de parenteses, para declarar mais de um argumento,
+e no segundo parametro de quantidade colocar [[dento]] colocar Colchetes dentro de Colchetes'''
 
-print(base.groupby('Tipo')["Curtidas"].mean())
+# print(base.groupby('Pessoas')[["Curtidas", "Comentários"]].mean())
+"""Aqui tiramos a prova que as publicações mais curtidas são as que contém pessoas!"""
+
+# print(base[base.Tipo == 'Foto'].groupby("Carrossel")[["Curtidas", "Comentários"]].mean())
+'''Aqui vemos que analisando apenas carrosel de fotos não temos tantas interações como pensavamos'''
+
+# print(base.groupby(['Pessoas', 'Campanhas'])[["Curtidas", "Comentários"]].mean())
+'''Nesse casopodemos perceber que as melhores publicações são as que possuem pessoas e as que possuem campanhas'''
+
+# print(base[base.Tipo == "Vídeo"])
+
+data = base[base.Tipo == "Vídeo"]
+print(data)
+
+'''Analisando este cenario podemos identificar que de acordo com os dados, a média de video foi positiva e o que esta'''
+
+'''Nesse caso podemos sugerir que a marca continue fazendo vídeos com posseoas e/ou em épocas de campanhas pois tem o 
+maior engajamento'''
+
+'''Para analisar a linha do tempo retirar o # do comentario, para conseguir analisar todos os passos individualmente!'''
+
+
 
